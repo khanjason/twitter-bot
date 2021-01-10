6ffb01cab820f99f22aba8e00cae6aa36c033464
Jason Khan <43140671+khanjason@users.noreply.github.com>
2021-01-10 15:03:37 +0000

    initial posting

---
+++ bot.py
@@ -0,0 +1,21 @@
+import tweepy 
+import datetime
+import os
+consumer_key = os.getenv('API_KEY')
+consumer_secret = os.getenv('API_SECRET')
+access_token = os.getenv('ACCESS_TOKEN')
+access_token_secret = os.getenv('ACCESS_SECRET')
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret) 
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+
+
+def publictweet():
+    dt=datetime.datetime.now()
+    
+    
+    if dt.hour==12 and dt.minute==0 and dt.second==0:
+        tweettopublish = ('Invite MUNchkin to your #discord server top.gg/bot/767330479757197323 and join support server discord.com/invite/xrhhD8b7AH #MUN #modelunitednations')
+        api.update_status(tweettopublish)
+publictweet()
